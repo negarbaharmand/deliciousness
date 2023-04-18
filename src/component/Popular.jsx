57153ff5dd8a337ca33d5351426371b8d325229a
Negar Baharmand <negarbaharmand@outlook.com>
2023-04-18 19:50:06 +0200

    Fix API issue

--- src/component/Popular.jsx
+++ src/component/Popular.jsx
@@ -1,6 +1,20 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function Popular() {
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const api = await fetch(
+      `https://api.spoonacular.com/recipes/random?apiKey=${
+        import.meta.env.VITE_API_KEY
+      }&number=9`
+    );
+    const data = await api.json();
+    console.log(data);
+  };
+
   return <div>Popular</div>;
 }
 
