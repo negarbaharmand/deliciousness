0a5b27de15301bb206fdefff9ce1c00e9dd74723
Negar Baharmand <negarbaharmand@outlook.com>
2023-04-20 11:07:31 +0200

    Cuisine Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import Pages from "./pages/Pages";
 import Category from "./component/Category";
+//BroserRouter gives the functionality to navigate over pages
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Category />
-      <Pages />
+      <BrowserRouter>
+        <Category />
+        <Pages />
+      </BrowserRouter>
     </div>
   );
 }
--- src/component/Category.jsx
+++ src/component/Category.jsx
@@ -2,27 +2,28 @@ import React from "react";
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
 import styled from "styled-components";
+//Importing NavLink instead of Link it gives a class called active and we can add styling
 import { NavLink } from "react-router-dom";
 
 function Category() {
   return (
     <List>
-      <div>
+      <NavLink to={"/cuisine/Italian"}>
         <FaPizzaSlice />
         <h4>Italian</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={"/cuisine/American"}>
         <FaHamburger />
         <h4>American</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={"/cuisine/Thai"}>
         <GiNoodles />
         <h4>Thai Food</h4>
-      </div>
-      <div>
+      </NavLink>
+      <NavLink to={"/cuisine/Japanese"}>
         <GiChopsticks />
         <h4>Japanese</h4>
-      </div>
+      </NavLink>
     </List>
   );
 }
