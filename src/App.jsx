47738cffc17a7b53dbd905bb1ebbf2febc82db15
Negar Baharmand <negarbaharmand@outlook.com>
2023-04-21 16:03:16 +0200

    Add logo to the App

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,18 @@ import Category from "./component/Category";
 //BroserRouter gives the functionality to navigate over pages
 import { BrowserRouter } from "react-router-dom";
 import Search from "./component/Search";
+import styled from "styled-components";
+import { GiKnifeFork } from "react-icons/gi";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+        <Nav>
+          <GiKnifeFork />
+          <Logo to={"/"}>deliciousss</Logo>
+        </Nav>
         <Search />
         <Category />
         <Pages />
@@ -15,5 +22,21 @@ function App() {
     </div>
   );
 }
+
+const Logo = styled(Link)`
+  text-decoration: none;
+  font-size: 1.5rem;
+  font-weight: 400;
+  font-family: "Lobster Two", cursive;
+`;
+const Nav = styled.div`
+  padding: 4rem 0rem;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    font-size: 2rem;
+  }
+`;
 
 export default App;
