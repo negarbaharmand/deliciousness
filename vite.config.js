5ab3e07df623ec6799e6299f6a9a1923880b514b
Negar Baharmand <negarbaharmand@outlook.com>
2023-10-01 21:20:07 +0200

    Update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/deliciousness/",
+  base: "/",
   assetsDir: "src",
   plugins: [react()],
 });
